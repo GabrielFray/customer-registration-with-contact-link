98e3fb99b0dfb33cb49e3b6d07d3092371d376d2
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-01 13:28:31 -0300

    feat: LoginPage and RegisterPage

---
+++ customer-registration-front-end/src/pages/LoginPage/index.tsx
@@ -0,0 +1,11 @@
+import LogInForm from "../../shared/components/LogInForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LogInForm />
+    </div>
+  );
+};
+
+export default LoginPage;
