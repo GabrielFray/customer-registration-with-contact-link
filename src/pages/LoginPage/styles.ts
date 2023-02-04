27416c49ae0dee024f69f88f41b2955930e95a59
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-03 21:50:48 -0300

    fix: Fixed bug login page

---
+++ src/pages/LoginPage/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ContentLoginPage = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+
+background-color: #8EC5FC;
+background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+width: 100vw;
+height: 100vh;
+
+    .logInCarousel {
+        display: flex;
+        background-color: #ffff;
+
+        width: 50%;
+        border-radius: 5px;
+    }
+   
+`
