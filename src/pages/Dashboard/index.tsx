231575972b55a56ddf8e891aa7f2a20d5cbffeae
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-09 22:56:44 -0300

    feat: Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import DashboardContacts from "../../shared/components/DashboardContacts";
+import DashboardHeader from "../../shared/components/DashboardHeader";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("@GetInTouch:token");
+  //   if (!token) {
+  //     navigate("/session");
+  //   }
+  // }, []);
   return (
-    <div>
-      <h1>Funcionou</h1>
-    </div>
+    <>
+      <DashboardHeader />
+      <DashboardContacts />
+    </>
   );
 };
 
