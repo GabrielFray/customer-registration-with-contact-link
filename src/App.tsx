69c73894241c9ad94959198d181d0799072a474c
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-09 22:56:07 -0300

    feat: Feature contact provider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import Routes from "./shared/routes";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import UserProvider from "./shared/context/UserContext";
+import ContactProviderContext from "./shared/context/ContactContext";
 
 const App = () => {
   return (
-    <UserProvider>
-      <Routes></Routes>
-      <ToastContainer
-        position={"top-right"}
-        autoClose={1000}
-        hideProgressBar={false}
-        closeOnClick={true}
-        pauseOnHover={true}
-        draggable={true}
-      />
-    </UserProvider>
+    <>
+      <UserProvider>
+        <ContactProviderContext>
+          <ToastContainer
+            position={"top-right"}
+            autoClose={1000}
+            hideProgressBar={false}
+            closeOnClick={true}
+            pauseOnHover={true}
+            draggable={true}
+          />
+          <Routes></Routes>
+        </ContactProviderContext>
+      </UserProvider>
+    </>
   );
 };
 
